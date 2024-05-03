c5ad7d3d17020c0849eec77fd069df8708ea0cfc
Tohatdk <159129122+Tohatdk@users.noreply.github.com>
2024-05-04 00:53:22 +0500

    format

--- lib/src/features/profile_feature/user_profile_page_bloc/profile_page_event.dart
+++ lib/src/features/profile_feature/user_profile_page_bloc/profile_page_event.dart
@@ -2,6 +2,6 @@ part of 'profile_page_bloc.dart';
 
 abstract class ProfilePageEvent {}
 
-class SignoutEvent extends ProfilePageEvent{}
+class SignoutEvent extends ProfilePageEvent {}
 
 class EditPhotoEvent extends ProfilePageEvent {}
