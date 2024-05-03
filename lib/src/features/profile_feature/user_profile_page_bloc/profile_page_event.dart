c4477ef5442db9c1fb4fb85f1e60809fc4e1c478
Tohatdk <159129122+Tohatdk@users.noreply.github.com>
2024-05-03 14:36:50 +0500

    edit events deleted

--- lib/src/features/profile_feature/user_profile_page_bloc/profile_page_event.dart
+++ lib/src/features/profile_feature/user_profile_page_bloc/profile_page_event.dart
@@ -2,10 +2,6 @@ part of 'profile_page_bloc.dart';
 
 abstract class ProfilePageEvent{}
 
-class EditNameEvent extends ProfilePageEvent{}
-
-class EditNumberEvent extends ProfilePageEvent{}
-
 class SignoutEvent extends ProfilePageEvent{}
 
 class EditPhotoEvent extends ProfilePageEvent{}
